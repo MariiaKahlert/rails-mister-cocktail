a854cf6ebaa58c51e2e7c4035fe6f9eb200d2121
Mariia Kahlert <mariakahlert@icloud.com>
2020-02-20 11:49:09 +0100

    Ingredients seeded

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -4,5 +4,4 @@ class Dose < ApplicationRecord
   validates :description, presence: true
   validates :cocktail, presence: true, uniqueness: { scope: :ingredient_id }
   validates :ingredient, presence: true, uniqueness: { scope: :cocktail_id }
-  # validates_uniqueness_of :cocktail_id, :ingredient_id
 end
