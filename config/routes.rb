754d0d74a8886a89cdd2e415584dcad25c4ce81c
Mariia Kahlert <mariakahlert@icloud.com>
2020-02-20 15:41:02 +0100

    DosesController with all actions and views implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails, except: %i[index edit update destroy]
+  resources :cocktails, except: %i[edit update destroy] do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: [:destroy]
 end
